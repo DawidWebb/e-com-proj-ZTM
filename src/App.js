e799e410c1a504ca83713422c6be9d51924af0d5
DawidWebb <tomasz.matras@developerweb.pl>
2022-10-31 09:58:53 +0100

    create authentication page component, set up firebase with google

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import NavigationBar from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-import SignIn from "./routes/sign-in/sign-in.components";
+import Authentication from "./routes/authentication/authentication.component";
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<NavigationBar />}>
 				<Route index element={<Home />} />
-				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/auth" element={<Authentication />} />
 			</Route>
 		</Routes>
 	);
