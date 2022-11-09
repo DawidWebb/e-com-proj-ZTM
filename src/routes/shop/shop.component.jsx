d2b3de72d5b7b07f4cd92f32cfc0440690fa70dd
DawidWebb <tomasz.matras@developerweb.pl>
2022-11-09 13:43:18 +0100

    add redux-thunk and set all async actions in thunh

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,19 +3,15 @@ import CategoriesPreview from "../categories-preview/categories-preview.componen
 import Category from "../category/category.component";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { setCategories } from "../../store/categories/categories.action";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { fetchCategoriesAsync } from "../../store/categories/categories.action";
+
 import { DivProductsContainer } from "./shop.styles";
 
 const Shop = () => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		const getCategoriesMap = async () => {
-			const categoriesArray = await getCategoriesAndDocuments();
-			dispatch(setCategories(categoriesArray));
-		};
-		getCategoriesMap();
+		dispatch(fetchCategoriesAsync());
 	}, []);
 
 	return (
