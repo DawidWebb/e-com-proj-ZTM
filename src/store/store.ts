3f74e15d492446f66d39fce99d76d72c5b052791
DawidWebb <tomasz.matras@developerweb.pl>
2022-11-16 09:45:01 +0100

    update store for typescript

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,31 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
+import { compose, createStore, applyMiddleware, Middleware } from "redux";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(Boolean);
+export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+	interface Window {
+		__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+	}
+}
+
+const middleWares = [process.env.NODE_ENV !== "production" && logger, thunk].filter((middleware): middleware is Middleware => Boolean(middleware));;
 
 const composeEnhancers =
 	(process.env.NODE_ENV !== "production" && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const composedEnhancers = composeEnhancers(applyMiddleware(...middleWares));
 
-const persistConfig = {
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+	whitelist: (keyof RootState)[];
+};
+
+const persistConfig: ExtendedPersistConfig = {
 	key: "root",
 	storage: storage,
 	whitelist: ["cart"],
